e96357ef0523b53ae1cffacdc03f09ac4df46cf2
varress <lahteenmakiessi@hotmail.fi>
2025-03-17 18:24:01 +0200

    Initial commit of Spring Boot application

---
+++ src/test/java/fi/secureprogramming/coursework/CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.secureprogramming.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
