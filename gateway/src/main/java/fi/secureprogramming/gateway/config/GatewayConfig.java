877bc112eb2af9bdd094b0c8e4c1bbc1248af695
varress <lahteenmakiessi@hotmail.fi>
2025-03-20 19:24:01 +0200

    Check request body in post request

--- gateway/src/main/java/fi/secureprogramming/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/fi/secureprogramming/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package fi.secureprogramming.gateway.config;
 
+import fi.secureprogramming.gateway.dto.ProductDTO;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,12 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("product-app", r -> r.path("/**")
+                .route("create-product", r -> r.path("/products")
+                        .and().method("POST")
+                        .and().readBody(ProductDTO.class, productDTO -> true)
+                        .uri("http://app:8080"))
+                .route("get-products", r -> r.path("/products")
+                        .and().method("GET")
                         .uri("http://app:8080"))
                 .build();
     }
---
+++ gateway/src/main/java/fi/secureprogramming/gateway/dto/ProductDTO.java
@@ -0,0 +1,28 @@
+package fi.secureprogramming.gateway.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public ProductDTO(Long id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
